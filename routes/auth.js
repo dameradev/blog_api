6f060ee5513e84946ddfc8a9f7e224b4fb08ae68
=DamjanRadev <radevdamjan@gmail.com>
2019-04-25 13:18:45 +0200

    Fixed bug with async-await

--- routes/auth.js
+++ routes/auth.js
@@ -10,12 +10,13 @@ router.post('/', async(req, res) => {
   const {error} = validate(req.body);
   if (error) return res.status(400).send(error.details[0].message);
 
-  let user = User.findOne({email: req.body.email});
+  let user = await User.findOne({email: req.body.email});
   if (!user) return res.status(400).send('Invalid email or password');
 
-  const validPassword  = bcrypt.compare(req.body.password, user.password);
+  const validPassword  = await bcrypt.compare(req.body.password, user.password);
   if (!validPassword) return res.status(400).send('Invalid email or password');
 
+  
   const token = user.generateAuthToken();
 
   res.header('x-auth-token', token).send(_.pick(user, ['_id', 'username', 'email' ]))
