d6ece677a6e2dd4dab8787817d42bb1387672383
=DamjanRadev <radevdamjan@gmail.com>
2019-04-25 13:15:18 +0200

    Completed auth module

--- models/user.js
+++ models/user.js
@@ -35,7 +35,7 @@ function validateUser(user) {
   const schema = {
     username: Joi.string().min(6).max(50).required(),
     email: Joi.string().min(6).max(50).email().required(),
-    password: Joi.string().min(6).required()
+    password: Joi.string().min(6).max(1024).required()
   }
 
   return Joi.validate(user, schema);
--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,33 @@
-const config = require('config');
+const Joi = require('joi');
+const _ = require('lodash');
 const bcrypt = require('bcrypt');
+const {User} = require('../models/user');
 const express = require('express');
 const router = express.Router();
 
 
 router.post('/', async(req, res) => {
+  const {error} = validate(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
 
+  let user = User.findOne({email: req.body.email});
+  if (!user) return res.status(400).send('Invalid email or password');
+
+  const validPassword  = bcrypt.compare(req.body.password, user.password);
+  if (!validPassword) return res.status(400).send('Invalid email or password');
+
+  const token = user.generateAuthToken();
+
+  res.header('x-auth-token', token).send(_.pick(user, ['_id', 'username', 'email' ]))
 });
+
+function validate(user) {
+  const schema = {
+    email: Joi.string().min(6).max(255).email().required(),
+    password: Joi.string().min(6).max(1024).required()
+  }
+
+  return Joi.validate(user, schema);
+}
+
+module.exports = router;
