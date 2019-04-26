349763b0143e2faf63026a5a0671aa26a149e23f
=DamjanRadev <radevdamjan@gmail.com>
2019-04-26 14:42:59 +0200

    Added console transports for uncaughtExeptions

--- startup/logging.js
+++ startup/logging.js
@@ -7,13 +7,14 @@ const logger = winston.createLogger({
     new winston.transports.File({ filename: 'error.log', level: 'error' }),
     new winston.transports.File({ filename: 'combined.log' }),
     new winston.transports.Console({ colorize: true, prettyPrint: true }),
-    //new winston.transports.MongoDB({db: 'mongodb://localhost/blog', collection: 'log'})
+    // new winston.transports.MongoDB({db: 'mongodb://localhost/blog', collection: 'log'})
   ]
 }); 
 
 const handleExeptions = function(){
   winston.exceptions.handle(
     new winston.transports.File({filename: 'uncaughtExeptions.log'}),
+    new winston.transports.Console({ colorize: true, prettyPrint: true }),
     new winston.transports.MongoDB({db: 'mongodb://localhost/blog', collection: 'log'})
   );
 
